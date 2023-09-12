0ffc5f3710bbc51339a78326d20911a13bb073e5
JuanMBrdch <juanmbordachar@gmail.com>
2023-09-12 19:35:14 -0300

    cosas

--- Assets/Scipts/Broom.cs
+++ Assets/Scipts/Broom.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class Broom : MonoBehaviour
 {
     public int[] EnemyLayer;
+    public int[] Enemy;
+    public int damage;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision != null) 
         {
+            
             Debug.Log(collision.gameObject.name);
             if (CheckCollisionLayers(collision, EnemyLayer)) 
             {
                 Destroy(collision.gameObject);
+            }
+            if (CheckCollisionLayers(collision, Enemy))
+            {
+                var healthController = collision.GetComponent<HealthController>();
+                if (healthController != null)
+                {
+                    healthController.DamageRecive(damage);
+                }
             }
         }
     }
